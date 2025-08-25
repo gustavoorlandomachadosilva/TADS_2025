7e59ad9fea566887a7fb35cfb63670a276111133
Gustavo Orlando <109483757+gustavoorlandomachadosilva@users.noreply.github.com>
2025-08-25 22:38:16 +0000

    [doc] included  a readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TADS_2025
-Repositorio destinado às aulas de estatistica
+# My first streamLit app
+
+This is a streamLit app that: 
+- Retrieve historical data from yahoo finance. 
+- Plot a time series using plotly. 
+
+You can see the app here[Here](https://tads2025-1-estatistica-historico-de-cotacoes.streamlit.app/).
--- functions/plot_ts.py
+++ functions/plot_ts.py
@@ -2,6 +2,16 @@ import yfinance as yf
 import plotly.express as px
 
 def plot(ticker: str):
+    """_summary_
+    Plot a time Series
+
+    Args:
+        ticker (str): the company ticker.
+
+    Returns:
+        _type_: return fig
+    """  
+
     data = yf.download(ticker, period='max', multi_level_index=False)
     df = data.reset_index()[['Date','Close']]   
 
